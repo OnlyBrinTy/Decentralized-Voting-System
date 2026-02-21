b939e11a96458a66c7fe10924ac7458124c9fb14
Roman <onlybrinty@gmail.com>
2026-02-21 15:50:11 +0100

    Key size changed

--- authority.py
+++ authority.py
@@ -14,18 +14,21 @@ class Authority:
     key's last two digits are always greater than 50. Anyone can verify
     if a key is for voting or registering.
     """
+    KEY_SIZE = 1024
 
     def __init__(self):
-        self._private_exponent, self.modulus = generate_keypair()
+        self._private_exponent, self.modulus = generate_keypair(key_size=self.KEY_SIZE)
         self.rsa_pss = RSASSA_PSS((self._private_exponent, self.modulus))
 
 
     def issue_voter_key(self):
-        voter_key = generate_keypair(voting_modulus=True)
-        voting_signature = self.rsa_pss.sign(voter_key)
+        voter_key = generate_keypair(voting_modulus=True, key_size=self.KEY_SIZE)
+        voting_signature = self.rsa_pss.sign(str(voter_key[1]))
+
         return voter_key, voting_signature
 
     def issue_participant_key(self):
-        participant_key = generate_keypair(participant_modulus=True)
-        participant_signature = self.rsa_pss.sign(participant_key)
+        participant_key = generate_keypair(participant_modulus=True, key_size=self.KEY_SIZE)
+        participant_signature = self.rsa_pss.sign(str(participant_key[1]))
+        
         return participant_key, participant_signature
